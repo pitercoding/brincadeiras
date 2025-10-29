268bc92d4d10a27084931872a396714cfecf2094
pitercoding <piterg.bio@gmail.com>
2025-10-29 17:10:51 +0100

    chore(backend): adiciona projeto Spring Boot inicial

---
+++ backend/src/main/java/com/brincadeiras/BrincadeirasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brincadeiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrincadeirasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrincadeirasBackendApplication.class, args);
+	}
+
+}
